753acd9ccd9836823df1e4b2fd8dfc6dc26f9c63
wenchuyang <writing_happy@163.com>
2018-05-30 23:21:18 +0800

    备份

--- js/main.js
+++ js/main.js
@@ -10,3 +10,13 @@ for(let i=0; i<liTags.length; i++){
     li.classList.remove('active')
   }
 }
+
+// manager页面使用bootstrap的js
+$('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+$('#myTabs a[href="#profile"]').tab('show') // Select tab by name
+$('#myTabs a:first').tab('show') // Select first tab
+$('#myTabs a:last').tab('show') // Select last tab
+$('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)
