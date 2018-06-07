97f77dfb3f70b14cf5b3d2a0b84ec610a35d2fac
wenchuyang <writing_happy@163.com>
2018-06-07 08:30:19 +0800

    连接数据库之前备份

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
-/* 导航栏鼠标浮上去显示子菜单*/
-let liTags = document.querySelectorAll('.navigation > ul > li')
-for(let i=0; i<liTags.length; i++){
-  liTags[i].onmouseenter = function(x){
-    let li = x.currentTarget
-    li.classList.add('active')
-  }
-  liTags[i].onmouseleave = function(x){
-    let li = x.currentTarget
-    li.classList.remove('active')
-  }
-}
+
 
 // manager页面使用bootstrap的js
 $('#myTabs a').click(function (e) {
