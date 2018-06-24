da5d08899aa355ef64e6cadd70082f70ab36d868
wenchuyang <writing_happy@163.com>
2018-06-24 17:07:52 +0800

    最终版

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# deep
+# 沉潜书屋管理系统的设计与实现
+## 安装使用说明
+1. 在navicate for MySQL里边运行deepread.sql文件，得到数据库deepread。这里使用的用户名和密码都是root。
+2. 在node.js环境中运行app.js文件，在浏览器端口打开localhost:3000即可进入网站首页。
+3. 进入网站首页之后可进行系列功能的演示。
--- routes/index.js
+++ routes/index.js
@@ -42,20 +42,35 @@ function getDateTime() {
   var day  = date.getDate();
   day = (day < 10 ? "0" : "") + day;
 
-  return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+  return year + "-" + month + "-" + day + "   " + hour + ":" + min + ":" + sec;
 
 }
+connection.connect();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  connection.query('select caid,name,discription from category', function(err, results, fields){
+    if(err){ console.log('查询出了问题 - ', err.message); return }
+    fs.writeFile('file/categoriesInfo.json', JSON.stringify(results), function(err){
+      if(err) throw err;
+    })
+  })
+  connection.query(`SELECT book.isbn,book.name as bookname,book.intro,book.author,book.publisher,book.price,book.reallyPrice,book.stock,book.image,book.printed,category.name as categoryname,category.caid
+    FROM book INNER JOIN category ON book.caid = category.caid
+    WHERE book.caid = category.caid`, function(err, results, fields){
+    if(err){ console.log('查询出了问题 - ', err.message); return }
+    fs.writeFile('file/bookInfo.json', JSON.stringify(results), function(err){
+      if(err) throw err;
+    })
+  })
   res.render('index', { 
-    title: 'Express',
-    categories: JSON.parse(fs.readFileSync('file/categoriesName.json', 'utf8'))
+    categories: JSON.parse(fs.readFileSync('file/categoriesName.json', 'utf8')),
+    book: JSON.parse(fs.readFileSync('file/bookInfo.json', 'utf8'))
   });
 });
 
-/*header里边的操作，把表中的数据放到文件里边。类别名字。*/
-connection.connect();
+
 
 
 
@@ -92,6 +107,7 @@ router.post('/managerSearchBook', function(req, res){
     })
     res.render('bookManager', { 
       search: true,
+      like: false,
       categories: JSON.parse(fs.readFileSync('file/categoriesInfo.json', 'utf8')),
       book: JSON.parse(fs.readFileSync('file/bookInfo.json', 'utf8'))
     });
@@ -117,6 +133,7 @@ router.post('/managerCategorySearchBook', function(req, res){
     })
     res.render('bookManager', { 
       search: true,
+      like: false,
       categories: JSON.parse(fs.readFileSync('file/categoriesInfo.json', 'utf8')),
       book: JSON.parse(fs.readFileSync('file/bookInfo.json', 'utf8'))
     });
@@ -141,6 +158,7 @@ router.post('/searchBook', function(req, res, next){
   })
   res.render('categories', { 
     search: true,
+    like: false,
     categories: JSON.parse(fs.readFileSync('file/categoriesInfo.json', 'utf8')),
     book: JSON.parse(fs.readFileSync('file/bookInfo.json', 'utf8'))
   });
@@ -162,6 +180,7 @@ router.post('/categorySearchBook', function(req, res){
   })
   res.render('categories', { 
     search: true,
+    like: false,
     categories: JSON.parse(fs.readFileSync('file/categoriesInfo.json', 'utf8')),
     book: JSON.parse(fs.readFileSync('file/bookInfo.json', 'utf8'))
   });
@@ -193,8 +212,105 @@ router.post('/readerCollect', function(req, res){
     res.send('收藏成功！') 
   }
 })
-
-
+router.post('/readyBuy', function(req, res){
+  connection.query('select rid,isbn,time from readyBuy', (err, results)=>{
+    if(err) throw err;
+    fs.writeFile('file/readyBuy.json', JSON.stringify(results), function(error){
+      if(error) throw error;
+      console.log('名字存在了file/readyBuy.json文件里边')
+    })
+  })
+  let readyBuy = JSON.parse(fs.readFileSync('file/readyBuy.json', 'utf8'))
+  let canrun = 1
+  readyBuy.forEach((collect)=>{
+    if(collect.isbn === req.body.isbn && collect.rid === req.cookies.loginId){
+      res.send('该商品已经在购物车中了')
+      canrun = 0
+    }
+  })
+  if(canrun){
+    let addSqlParams = [req.cookies.loginId, req.body.isbn, getDateTime()];
+    connection.query('INSERT INTO readyBuy(rid,isbn,time) VALUES(?,?,?)',addSqlParams,function (err, result) {
+      if(err){
+        console.log('[INSERT ERROR] - ',err.message);
+        return;
+      }   
+    });
+    res.send('添加购物车成功！') 
+  }
+})
+router.get('/favorite.html', function(req, res, next) {
+  connection.query('select caid,name,discription from category', function(err, results, fields){
+    if(err){ console.log('查询出了问题 - ', err.message); return }
+    fs.writeFile('file/categoriesInfo.json', JSON.stringify(results), function(err){
+      if(err) throw err;
+    })
+  })
+  connection.query(`SELECT
+    book.isbn,
+    book.name as bookname,
+    book.intro,
+    book.author,
+    book.publisher,
+    book.price,
+    book.reallyPrice,
+    book.stock,
+    book.image,
+    book.printed,
+    readercollect.time,
+    category.name as categoryname,
+    book.caid
+    FROM
+    book
+    INNER JOIN readercollect ON book.isbn = readercollect.isbn
+    INNER JOIN category ON book.caid = category.caid
+    WHERE
+    readercollect.rid = ? AND
+    book.isbn = readercollect.isbn AND
+    book.caid = category.caid
+    `,[req.cookies.loginId], function(err, results, fields){
+    if(err){ console.log('查询出了问题 - ', err.message); return }
+    fs.writeFile('file/likeBookInfo.json', JSON.stringify(results), function(err){
+      if(err) throw err;
+    })
+  })
+  res.render('categories', { 
+    search: true,
+    like: true,
+    categories: JSON.parse(fs.readFileSync('file/categoriesInfo.json', 'utf8')),
+    book: JSON.parse(fs.readFileSync('file/likeBookInfo.json', 'utf8'))
+  });
+});
+router.post('/cancelBuyBook', function(req,res){
+  connection.query('delete from readyBuy where isbn = ? AND rid = ?',[req.body.isbn, req.cookies.loginId],function(err, results){
+    if(err) throw err;
+  })
+  res.send('取消成功')
+})
+router.post('/cancelAllBuyBook', function(req,res){
+  connection.query('delete from readyBuy where rid = ?',[req.cookies.loginId],function(err, results){
+    if(err) throw err;
+  })
+  res.send('取消成功')
+})
+router.post('/submitOrder', function(req, res){
+  let orderId = generateUUID()
+  connection.query('INSERT INTO orders(ordersid,money,time,rid,finished) VALUES(?,?,?,?,?)',
+    [orderId, req.body.money, getDateTime(), req.cookies.loginId, 0],function(err, results){
+    if(err) throw err;
+  })
+  let isbns = JSON.parse(req.body.isbns)
+  isbns.forEach((isbn)=>{
+    connection.query('INSERT INTO orderstobook(isbn,orderid) VALUES(?,?)',
+      [isbn, orderId],function(err, results){
+        if(err) throw err;
+    })
+  })
+  connection.query('delete from readyBuy where rid = ?',[req.cookies.loginId],function(err, results){
+    if(err) throw err;
+  })
+  res.send('订单提交成功')  
+})
 
 router.get('/footer.html', function(req, res, next) {
   res.render('footer', { });
@@ -205,17 +321,228 @@ router.get('/categories.html', function(req, res, next) {
 });
 
 router.get('/cart.html', function(req, res, next) {
-  res.render('cart', { });
+  connection.query(`SELECT
+    readybuy.isbn,
+    book.name,
+    book.reallyPrice,
+    book.price
+    FROM
+    readybuy ,
+    book
+    WHERE
+    readybuy.rid = ? AND
+    readybuy.isbn = book.isbn
+  `,[req.cookies.loginId],function(err, results){
+    if(err) throw err;
+    fs.writeFile('file/ordersBookInfo.json', JSON.stringify(results), function(err){
+      if(err) throw err;
+    })
+  })
+  res.render('cart', { 
+    isManager: false,
+    book: JSON.parse(fs.readFileSync('file/ordersBookInfo.json', 'utf8'))
+  });
 });
+router.get('/cartManager.html', function(req, res, next){
+  connection.query(`SELECT
+    orders.ordersid,
+    orders.money,
+    orders.time,
+    orders.finished,
+    reader.username,
+    reader.membership,
+    orders.rid
+    FROM
+    orders ,
+    reader
+    WHERE
+    orders.rid = reader.rid
+    `,function(err, results){
+    if(err) throw err;
+    fs.writeFile('file/ordersInfo.json', JSON.stringify(results), function(err){
+      if(err) throw err;
+    })
+  })
+  res.render('cart', {
+    isManager: true,
+    orders: JSON.parse(fs.readFileSync('file/ordersInfo.json', 'utf8'))
+  })
+})
+router.post('/setMembership',function(req, res, next){
+  connection.query('UPDATE reader SET membership = 1 WHERE rid=?',
+  [req.body.rid], function(err){
+    if(err) throw err;
+  })
+  res.send('设置成功')
+})
+router.post('/finishOrder',function(req, res, next){
+  connection.query('UPDATE orders SET finished=1,wid=? WHERE rid=? AND ordersid=?',
+  [req.cookies.loginId, req.body.rid, req.body.ordersid], function(err){
+    if(err) throw err;
+  })
+  connection.query(`SELECT
+    orders.time,
+    orders.ordersid,
+    orders.rid,
+    orderstobook.isbn,
+    book.name
+    FROM
+    orders ,
+    orderstobook ,
+    book
+    WHERE
+    orders.ordersid = ? AND
+    orders.ordersid = orderstobook.orderid AND
+    book.isbn = orderstobook.isbn
+    `,[req.body.ordersid],function(err, results){
+    if(err) throw err;
+    fs.writeFile('file/ordersToReaderbuy.json', JSON.stringify(results), function(err){
+      if(err) throw err;
+    })
+  })
+  let booksInfo = JSON.parse(fs.readFileSync('file/ordersToReaderbuy.json', 'utf8'))
+  booksInfo.forEach((bookInfo)=>{
+    connection.query('INSERT INTO readerbuy(rid,isbn,time) VALUES(?,?,?)',
+      [bookInfo.rid, bookInfo.isbn, bookInfo.time],function(err, results){
+        if(err) throw err;
+    })
+  })
+  res.send('设置成功')
+})
+
 
 router.get('/bookDetail.html', function(req, res, next) {
   res.render('bookDetail', { });
 });
 
-router.get('/favorite.html', function(req, res, next) {
-  res.render('favorite', { });
+/*书籍评论的处理那一块儿*/
+router.post('/commentDetail.html', function(req, res){
+  connection.query('SELECT name,isbn from book WHERE isbn=?',[req.body.isbn],function(err,results){
+    if(err) throw err;
+    fs.writeFile('file/readyCommentBookInfo.json', JSON.stringify(results), function(error){
+      if(error) throw error;
+    })
+  })
+  res.render('commentDetail', {
+    commentState: 'createComment',
+    book: JSON.parse(fs.readFileSync('file/readyCommentBookInfo.json', 'utf8'))
+  })
+})
+router.post('/createComment', function(req, res, next){
+  connection.query('INSERT INTO comment(cmid,title,content,isbn,rid,time) VALUES(?,?,?,?,?,?)',
+  [generateUUID(),req.body.title,req.body.content,req.body.isbn,req.cookies.loginId,getDateTime()],function (err, result) {
+    if(err){
+      console.log('[INSERT ERROR] - ',err.message);
+      return;
+    }   
+  })
+  res.send('评论成功')
+})
+router.get('/personal.html', function(req, res, next) {
+  if(req.cookies.loginId&&req.cookies.isManager!=1){
+    connection.query('SELECT * from reader WHERE rid=?',[req.cookies.loginId],function(err,results){
+      if(err) throw err;
+      fs.writeFile('file/readerInfo.json', JSON.stringify(results), function(error){
+        if(error) throw error;
+      })
+    })
+    connection.query(`SELECT
+      comment.cmid,
+      comment.title,
+      comment.content,
+      comment.rid,
+      comment.isbn,
+      comment.time,
+      book.name AS bookname,
+      reader.username
+      FROM
+      comment
+      INNER JOIN book ON comment.isbn = book.isbn
+      INNER JOIN reader ON comment.rid = reader.rid
+      WHERE
+      comment.rid = ? AND
+      comment.isbn = book.isbn AND
+      comment.rid = reader.rid`,[req.cookies.loginId],function(err,results){
+      if(err) throw err;
+      fs.writeFile('file/commentInfo.json', JSON.stringify(results), function(error){
+        if(error) throw error;
+      })
+    })
+    res.render('personal', { 
+      isManager: false,
+      user: JSON.parse(fs.readFileSync('file/readerInfo.json', 'utf8')),
+      comments: JSON.parse(fs.readFileSync('file/commentInfo.json', 'utf8'))
+    })
+  }else{
+    res.send('你没有登录哦')
+  }
 });
-
+router.post('/displayComment', function(req, res){
+  res.render('commentDetail',{
+    commentState: 'displayComment',
+    commentId: req.body.cmid,
+    commentTitle: req.body.title,
+    commentTime: req.body.time,
+    commentBook: req.body.bookname,
+    commentContent: req.body.content
+  })
+})
+router.post('/managerDisplayComment', function(req, res){
+  res.render('commentDetail',{
+    commentState: 'managerDisplayComment',
+    commentId: req.body.cmid,
+    commentTitle: req.body.title,
+    commentTime: req.body.time,
+    commentBook: req.body.bookname,
+    commentContent: req.body.content
+  })
+})
+router.post('/changeComment', function(req, res){
+  connection.query('UPDATE comment SET title=?,content=? WHERE cmid=?',
+  [req.body.title, req.body.content, req.body.cmid], function(err, result){
+    if(err) throw err;
+    res.send('修改成功')
+  })
+})
+router.post('/deleteComment', function(req, res){
+  connection.query('delete FROM comment WHERE cmid=?',
+  [req.body.cmid], function(err, result){
+    if(err) throw err;
+    res.send('删除成功')
+  })
+})
+/*管理员对书评的管理*/
+router.get('/commentManager.html', function(req, res){
+  if(req.cookies.isManager == 1){
+    connection.query(`SELECT
+      comment.cmid,
+      comment.title,
+      comment.content,
+      comment.rid,
+      comment.isbn,
+      comment.time,
+      book.name AS bookname,
+      reader.username
+      FROM
+      comment
+      INNER JOIN book ON comment.isbn = book.isbn
+      INNER JOIN reader ON comment.rid = reader.rid
+      WHERE
+      comment.isbn = book.isbn AND
+      comment.rid = reader.rid`,function(err,results){
+      if(err) throw err;
+      fs.writeFile('file/commentInfo.json', JSON.stringify(results), function(error){
+        if(error) throw error;
+      })
+    })
+    res.render('personal', { 
+      isManager: true,
+      comments: JSON.parse(fs.readFileSync('file/commentInfo.json', 'utf8'))
+    })
+  }else{
+    res.send('你不是管理员')
+  }
+})
 
 /*登录注册那一块儿*/ 
 router.get('/loginAndRegister.html', function(req, res, next) {
@@ -239,7 +566,7 @@ router.post('/saveUser', function(req, res, next){
     }
   })
   if(add){
-    let  addSqlParams = [generateUUID(), req.body.name, req.body.phone, req.body.password, '1'];
+    let  addSqlParams = [generateUUID(), req.body.name, req.body.phone, req.body.password, '0'];
     connection.query('INSERT INTO reader(rid,username,phone,password,membership) VALUES(?,?,?,?,?)',addSqlParams,function (err, result) {
       if(err){
         console.log('[INSERT ERROR] - ',err.message);
@@ -265,7 +592,9 @@ router.post('/login', function(req, res, next){
         res.cookie('loginName', user.userName);
         res.cookie('loginId', user.rid) 
         res.cookie('isManager', 0)
-        res.render('index')
+        res.render('index', { 
+          book: JSON.parse(fs.readFileSync('file/bookInfo.json', 'utf8'))
+        });
       }else{
         res.send('密码错误！')
       }
@@ -295,7 +624,9 @@ router.post('/managerLogin', function(req, res){
         res.cookie('loginName', user.name);
         res.cookie('loginId', user.wid) 
         res.cookie('isManager', 1)
-        res.render('index')
+        res.render('index', { 
+          book: JSON.parse(fs.readFileSync('file/bookInfo.json', 'utf8'))
+        });
       }else{
         res.send('密码错误！')
       }
@@ -413,6 +744,8 @@ router.post('/deleteBook', function(req, res){
 })
 
 
+
+
 router.get('/manager.html', function(req, res, next) {
   if(req.cookies.isManager == 1){
     connection.query('select caid,name,discription from category', function(err, results, fields){
@@ -456,19 +789,8 @@ router.post('/changeBook', function(req, res){
     res.send('修改成功')
   })
 })
-router.post('/deleteBook', function(req, res){
-  connection.query('delete from book where isbn = ?', req.body.isbn, function(err, result){
-    if(err) throw err;
-    res.send('删除成功')
-  })
-})
 
-router.get('/personal.html', function(req, res, next) {
-  if(req.cookies.loginId&&req.cookies.isManager!=1){
-    res.render('personal', { })
-  }else{
-    res.send('你没有登录哦')
-  }
-});
+
+
 
 module.exports = router;
